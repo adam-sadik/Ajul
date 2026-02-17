d5be30f3e71b900770ba6fc50787fdd53e7dc5a5
‹adam-sadik <adamghali23112007@gmail.com>
2026-02-17 17:16:51 +0400

    3.3. Interface TileKind + 3.7. Interface TileSource

--- src/ch/epfl/ajul/TileKind.java
+++ src/ch/epfl/ajul/TileKind.java
@@ -3,25 +3,47 @@ package ch.epfl.ajul;
 import java.util.List;
 import java.util.random.RandomGenerator;
 
-public interface TileKind {
-    public final static TileKind A;
-    public final static TileKind B;
-    public final static TileKind C;
-    public final static TileKind D;
-    public final static TileKind E;
-    public final static TileKind FIRST_PLAYER_MAKER;
+public sealed interface TileKind {
+    TileKind A = Colored.A;
+    TileKind B = Colored.B;
+    TileKind C = Colored.C;
+    TileKind D = Colored.D;
+    TileKind E = Colored.E;
+    TileKind FIRST_PLAYER_MAKER = FirstPlayerMarker.FIRST_PLAYER_MAKER;
     List<TileKind> ALL = List.of(A,B,C,D,E,FIRST_PLAYER_MAKER);
-    int COUNT = 6;
-    public abstract int index();
-    public abstract int tilesCount();
+    int COUNT = ALL.size();
+    int index();
+    int tilesCount();
     enum Colored implements TileKind{
         A,B,C,D,E;
-        int COUNT = 5;
+        public static final List<Colored> ALL = List.of(values());
+        public static final int COUNT = ALL.size();
+        @Override
+        public int index(){
+            return ordinal();
+        }
+        @Override
+        public int tilesCount(){
+            return 20;
+        }
         public static void shuffle(Colored[] tiles, RandomGenerator randomGenerator){
-
+            for (int i =0; i<= tiles.length - 2; ++i){
+                int j = randomGenerator.nextInt(i, tiles.length);
+                Colored temp = tiles[i];
+                tiles[i] = tiles[j];
+                tiles[j] = temp;
+            }
         }
     }
     enum FirstPlayerMarker implements TileKind{
-
+        FIRST_PLAYER_MARKER;
+        @Override
+        public int index(){
+            return 5;
+        }
+        @Override
+        public int tilesCount(){
+            return 1;
+        }
     }
 }
---
+++ src/ch/epfl/ajul/TileSource.java
@@ -0,0 +1,36 @@
+package ch.epfl.ajul;
+
+import java.util.List;
+
+public sealed interface TileSource {
+    TileSource CENTER_AREA = CenterArea.CENTER_AREA;
+    TileSource FACTORY_1 = Factory.FACTORY_1;
+    TileSource FACTORY_2 = Factory.FACTORY_2;
+    TileSource FACTORY_3 = Factory.FACTORY_3;
+    TileSource FACTORY_4 = Factory.FACTORY_4;
+    TileSource FACTORY_5 = Factory.FACTORY_5;
+    TileSource FACTORY_6 = Factory.FACTORY_6;
+    TileSource FACTORY_7 = Factory.FACTORY_7;
+    TileSource FACTORY_8 = Factory.FACTORY_8;
+    TileSource FACTORY_9 = Factory.FACTORY_9;
+    List<TileSource> ALL = List.of(CENTER_AREA, FACTORY_1,FACTORY_2,FACTORY_3,FACTORY_4,FACTORY_5,FACTORY_6,FACTORY_7,FACTORY_8,FACTORY_9);
+    int COUNT = ALL.size();
+    int index();
+    enum CenterArea implements TileSource{
+        CENTER_AREA;
+        @Override
+        public int index(){
+            return 0;
+        }
+    }
+    enum Factory implements TileSource{
+        FACTORY_1,FACTORY_2,FACTORY_3,FACTORY_4,FACTORY_5,FACTORY_6,FACTORY_7,FACTORY_8,FACTORY_9;
+        public final static int TILES_PER_FACTORY = 4;
+        public static final List<TileSource.Factory> ALL = List.of(values());
+        public static final int COUNT = ALL.size();
+        @Override
+        public int index(){
+            return ordinal() + 1;
+        }
+    }
+}
