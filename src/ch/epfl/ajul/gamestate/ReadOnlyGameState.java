e31ec14449d54d6621e1d4a938354f7d7b8d6435
‹adam-sadik <adamghali23112007@gmail.com>
2026-03-13 15:33:22 +0100

    Asserts commentes

--- src/ch/epfl/ajul/gamestate/ReadOnlyGameState.java
+++ src/ch/epfl/ajul/gamestate/ReadOnlyGameState.java
@@ -22,15 +22,15 @@ public interface ReadOnlyGameState {
     PlayerId currentPlayerId();
 
     default ImmutableGameState immutable() {
-        return new ImmutableGameState( game(),
+        return new ImmutableGameState(game(),
                 pkTileBag(),
                 (ImmutableIntArray) pkTileSources(),
                 pkUniqueTileSources(),
                 (ImmutableIntArray) pkPlayerStates(),
-                currentPlayerId() );
+                currentPlayerId());
     }
 
-    default List<PlayerId> playerIds(){
+    default List<PlayerId> playerIds() {
         return game().playerIds();
     }
 
@@ -40,8 +40,8 @@ public interface ReadOnlyGameState {
 
     default boolean isGameOver() {
         boolean isRowFull = false;
-        for (int i = 0; i < playerIds().size() ; ++i) {
-            if(PkWall.hasFullRow(PkPlayerStates.pkWall(pkPlayerStates(),PlayerId.ALL.get(i)))) {
+        for (int i = 0; i < playerIds().size(); ++i) {
+            if (PkWall.hasFullRow(PkPlayerStates.pkWall(pkPlayerStates(), PlayerId.ALL.get(i)))) {
                 isRowFull = true;
                 break;
             }
@@ -49,9 +49,9 @@ public interface ReadOnlyGameState {
         return isRoundOver() && isRowFull;
     }
 
-    default int pkDiscardedTiles(){
+    default int pkDiscardedTiles() {
 
-        int sources = PkTileSet.EMPTY ;
+        int sources = PkTileSet.EMPTY;
         int wall = PkTileSet.EMPTY;
         int floor = PkTileSet.EMPTY;
         int patterns = PkTileSet.EMPTY;
@@ -61,47 +61,53 @@ public interface ReadOnlyGameState {
         int total;
 
         for (int i = 0; i < pkTileSources().size(); ++i) {
-            sources = PkTileSet.union(sources,pkTileSources().get(i) );
+            sources = PkTileSet.union(sources, pkTileSources().get(i));
         }
 
-        unionBagAndSources = PkTileSet.union(pkTileBag(), sources );
+        unionBagAndSources = PkTileSet.union(pkTileBag(), sources);
 
         for (int i = 0; i < playerIds().size(); ++i) {
             floor = PkTileSet.union(floor, PkPlayerStates.pkPatterns(pkPlayerStates(), PlayerId.ALL.get(i)));
         }
 
         for (int i = 0; i < playerIds().size(); i++) {
-            patterns = PkTileSet.union(patterns,PkPlayerStates.pkFloor(pkPlayerStates(), PlayerId.ALL.get(i)));
+            patterns = PkTileSet.union(patterns, PkPlayerStates.pkFloor(pkPlayerStates(), PlayerId.ALL.get(i)));
         }
 
-        patternsAndFloor = PkTileSet.union ( floor, patterns);
+        patternsAndFloor = PkTileSet.union(floor, patterns);
 
-        for (int i = 0; i < playerIds().size() ; i++) {
+        for (int i = 0; i < playerIds().size(); i++) {
             wall = PkTileSet.union(wall, PkPlayerStates.pkWall(pkPlayerStates(), PlayerId.ALL.get(i)));
         }
 
-        unionWallAndPatternsAndFloor = PkTileSet.union(wall,patternsAndFloor);
+        unionWallAndPatternsAndFloor = PkTileSet.union(wall, patternsAndFloor);
         total = PkTileSet.union(unionBagAndSources, unionWallAndPatternsAndFloor);
 
-        return PkTileSet.difference(PkTileSet.FULL,total);
+        return PkTileSet.difference(PkTileSet.FULL, total);
     }
 
-    default int validMoves(short[] destination) {
-        assert destination.length >= Move.MAX_MOVES;
-        int index = currentPlayerId().ordinal();
+    private int generateMoves(int allowedSources, short[] destination) {
         int i = 0;
-        for (TileSource source : TileSource.ALL) {
-            for (TileKind.Colored color : TileKind.Colored.ALL) {
-                for (TileDestination dest : TileDestination.ALL) {
-                    boolean isPatternFull = PkPatterns.isFull(PkPlayerStates.pkPatterns(pkPlayerStates(), currentPlayerId()), (TileDestination.Pattern) dest);
+        PlayerId me = currentPlayerId();
+        int myPatterns = PkPlayerStates.pkPatterns(pkPlayerStates(), me);
+        int myWall = PkPlayerStates.pkWall(pkPlayerStates(), me);
 
-                    if ((dest instanceof TileDestination.Pattern) && !isPatternFull) {
-                        if (color == PkPatterns.color(PkPlayerStates.pkPatterns(pkPlayerStates(), currentPlayerId()), (TileDestination.Pattern) dest))
-                            destination[i] = PkMove.pack(source, color, dest);
-                        ++i;
-                    } else if (dest instanceof TileDestination.Floor) {
-                        destination[i] = PkMove.pack(source, color, dest);
-                        ++i;
+        for (TileSource source : TileSource.ALL) {
+            if (PkIntSet32.contains(allowedSources, source.index())) {
+                int sourceTiles = pkTileSources().get(source.index());
+                for (TileKind.Colored color : TileKind.Colored.ALL) {
+                    if (PkTileSet.countOf(sourceTiles, color) != 0) {
+                        for (TileDestination dest : TileDestination.ALL) {
+                            if (dest instanceof TileDestination.Pattern pDest) {
+                                if (!PkPatterns.isFull(myPatterns, pDest)
+                                        && PkPatterns.canContain(myPatterns, pDest, color)
+                                        && !PkWall.hasTileAt(myWall, pDest, color)) {
+                                    destination[i++] = PkMove.pack(source, color, dest);
+                                }
+                            } else if (dest instanceof TileDestination.Floor) {
+                                destination[i++] = PkMove.pack(source, color, dest);
+                            }
+                        }
                     }
                 }
             }
@@ -109,8 +115,16 @@ public interface ReadOnlyGameState {
         return i;
     }
 
+    default int validMoves(short[] destination) {
+        int allSources = PkIntSet32.EMPTY;
+        for (TileSource source : TileSource.ALL) {
+            allSources = PkIntSet32.add(allSources, source.index());
+        }
+        return generateMoves(allSources, destination);
+    }
 
-
-
-
+    default int uniqueValidMoves(short[] destination) {
+        return generateMoves(pkUniqueTileSources(), destination);
+    }
 }
+
