702d90da5742e8e1dd479803532a8e264ab3fa81
Rayane Taoufik Benchekroun <rayane.taoufikbenchekroun@epfl.ch>
2026-03-31 10:33:21 +0200

    p

--- src/ch/epfl/ajul/gamestate/packed/PkIntSet32.java
+++ src/ch/epfl/ajul/gamestate/packed/PkIntSet32.java
@@ -4,7 +4,6 @@ package ch.epfl.ajul.gamestate.packed;
 /// Classe utilitaire permettant de manipuler des ensembles d'entiers empaquetés dans un `int`.
 /// L'ensemble peut contenir des entiers compris entre 0 et 31 inclus.
 /// Le bit d'index i correspond à la présence de l'entier i dans l'ensemble.
-
 /// @author Rayane Taoufik Benchekroun (412052)
 /// @author Adam Ghali SADIK (412029)
 public final class PkIntSet32 {
@@ -16,8 +15,8 @@ public final class PkIntSet32 {
     /// @param pkIntSet32 L'ensemble empaqueté.
     /// @param i L'entier dont on veut vérifier la présence (doit être entre 0 et 31).
     /// @return Vrai si l'entier i est présent dans l'ensemble.
-    public static boolean contains (int pkIntSet32, int i) {
-        //assert i >= 0 && i < Integer.SIZE;
+    public static boolean contains(int pkIntSet32, int i) {
+        assert i >= 0 && i < Integer.SIZE;
         return ((pkIntSet32 >>> i) & 1) == 1;
     }
 
@@ -25,7 +24,7 @@ public final class PkIntSet32 {
     /// @param pkIntSet32a L'ensemble supposé contenant.
     /// @param pkIntSet32b L'ensemble dont on cherche les éléments.
     /// @return Vrai si pkIntSet32a est un sur-ensemble de pkIntSet32b.
-    public static boolean containsAll (int pkIntSet32a, int pkIntSet32b){
+    public static boolean containsAll(int pkIntSet32a, int pkIntSet32b) {
         return (pkIntSet32a & pkIntSet32b) == pkIntSet32b;
     }
 
@@ -33,8 +32,8 @@ public final class PkIntSet32 {
     /// @param pkIntSet32 L'ensemble d'origine.
     /// @param i L'entier à ajouter (doit être entre 0 et 31).
     /// @return Un nouvel ensemble empaqueté contenant i.
-    public static int add (int pkIntSet32, int i){
-        //assert i >= 0 && i < Integer.SIZE;
+    public static int add(int pkIntSet32, int i) {
+        assert i >= 0 && i < Integer.SIZE;
         return pkIntSet32 | (1 << i);
     }
 
@@ -42,8 +41,8 @@ public final class PkIntSet32 {
     /// @param pkIntSet32 L'ensemble d'origine.
     /// @param i L'entier à retirer (doit être entre 0 et 31).
     /// @return Un nouvel ensemble empaqueté ne contenant plus i.
-    public static int remove (int pkIntSet32, int i){
-        //assert i >= 0 && i < Integer.SIZE;
+    public static int remove(int pkIntSet32, int i) {
+        assert i >= 0 && i < Integer.SIZE;
         return pkIntSet32 & ~(1 << i);
     }
 
